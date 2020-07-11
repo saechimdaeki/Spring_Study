a7b70a15f9dd95930ce4a8a2546e46c8ad2f7471
김준성 <anima94@naver.com>
2020-07-11 12:26:53 +0900

    directory And Web Fix :airplane:

---
+++ 11. MVC2 메시지, 커맨드 객체 검증/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import controller.RegisterController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.MemberRegisterService;
+import survey.SurveyController;
+
+
+@Configuration
+public class ControllerConfig {
+	@Autowired
+	private MemberRegisterService memberRegSvc;
+	@Bean
+	public RegisterController registerController() {
+		RegisterController controller=new RegisterController();
+		controller.setMemberRegisterService(memberRegSvc);
+		return controller;
+	}
+	@Bean
+	public SurveyController surveyController(){
+		return new SurveyController();
+	}
+
+}
---
+++ 11. MVC2 메시지, 커맨드 객체 검증/src/main/java/survey/AnswereData.java
@@ -0,0 +1,24 @@
+package survey;
+
+import java.util.List;
+
+public class AnswereData {
+    private List<String> responses;
+    private Respondent res;
+
+    public List<String> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(List<String> responses) {
+        this.responses = responses;
+    }
+
+    public Respondent getRes() {
+        return res;
+    }
+
+    public void setRes(Respondent res) {
+        this.res = res;
+    }
+}
